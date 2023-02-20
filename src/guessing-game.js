b5a639242030e9b44e9a12462e4fe5ffad227368
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2023-02-20 15:09:44 +0300

    feat: solved the guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+     
+    }
 
     setRange(min, max) {
+this.minNum = min
+this.maxNum = max
 
     }
 
     guess() {
-
+      return  Math.round( (this.maxNum + this.minNum)/2)
     }
 
     lower() {
-
+        return this.maxNum = (Math.round((this.maxNum + this.minNum) / 2))
     }
 
     greater() {
-
+        return this.minNum = (Math.round((this.maxNum + this.minNum) / 2))
     }
 }
 
